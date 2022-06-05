55127a8d59936dc745e1d882336062c2cb512ab3
GabrielleHoyte <gabriellehoyte@gmail.com>
2022-06-05 16:37:22 -0400

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+  url: "mongodb+srv://GabrielleHoyte:Password0@cluster0.ioxl7.mongodb.net/cassCafe?retryWrites=true&w=majority",
+  dbName: "cassCafe",
+};
